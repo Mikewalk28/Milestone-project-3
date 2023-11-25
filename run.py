1ec2c339a2e8d706f5d5e10cc50d0c780a0e0f93
Mikewalk28 <>
2023-11-25 11:14:19 +0000

    adding base files to read front end from run.py

--- run.py
+++ run.py
@@ -1,8 +1,17 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+from overheadmanager import app
 import gspread
 from google.oauth2.service_account import Credentials
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
+    )
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
