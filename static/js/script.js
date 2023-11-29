717ebb423d150c7d3faafd1895e21ad5e98f5a78
Mikewalk28 <>
2023-11-29 19:04:48 +0000

    add link to footer

--- static/js/script.js
+++
@@ -1,5 +0,0 @@
-let timesheets = document.getElementById('timesheet');
-timesheets.addEventListener('click', function () {
-    timesheets.classList.remove('hide');
-    homeSection.classList.add('hide');
-});
