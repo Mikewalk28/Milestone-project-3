93817e219e30e24bbf027df701e9b999758ec768
Mikewalk28 <>
2023-11-30 19:35:55 +0000

    amending documents to get bar chart working on reports tab

--- overheadmanager/routes.py
+++ overheadmanager/routes.py
@@ -2,7 +2,8 @@ from flask import render_template
 from overheadmanager import app, db
 import gspread
 from google.oauth2.service_account import Credentials
-
+import pandas as pd
+​
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -12,29 +13,41 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Milestone-project-3')
-
+​
 fy22 = SHEET.worksheet('FY22/23')
 november = SHEET.worksheet('November')
-
+​
 data = fy22.get_all_values()
 data2 = november.get_all_values()
-
-print(data)
-print(data2)
-
-
+​
+​
+​
 @app.route("/")
 def home():
     return render_template("home.html")
-
-
+​
+​
 @app.route("/reports")
 def reports():
     fy22 = SHEET.worksheet('FY22/23')
-    data = fy22.get_all_values()
-    return render_template("reports.html", sheet_data=data)
-
-
+    headers = ["Sector", "Total"]
+    values = fy22.get_all_values()
+    sectors = [value[0] for value in values[1:]]
+    total = [value[12] for value in values[1:]]
+    return render_template("reports.html", sheet_data=[sectors, total])
+​
+​
+@app.route("/reports-data")
+def reports_data():
+    fy22 = SHEET.worksheet('FY22/23')
+    headers = ["Sector", "Total"]
+    values = fy22.get_all_values()
+    sectors = [value[0] for value in values[1:]]
+    total = [value[12] for value in values[1:]]
+    return render_template("reports.html", bar_data=[{'x': sectors, 'y': total, 'type': 'bar'}])
+​
+​
+​
 @app.route("/timesheets")
 def timesheets():
     november = SHEET.worksheet('November')
