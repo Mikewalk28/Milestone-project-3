7a0b06c22d25f94e3770dd005e6b905cf69f5301
Mikewalk28 <>
2023-12-01 19:44:19 +0000

    winding back pages by 2 commits as graph not loading and repasting in current commit

--- overheadmanager/routes.py
+++ overheadmanager/routes.py
@@ -3,7 +3,7 @@ from overheadmanager import app, db
 import gspread
 from google.oauth2.service_account import Credentials
 import pandas as pd
-​
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -13,20 +13,19 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Milestone-project-3')
-​
+
 fy22 = SHEET.worksheet('FY22/23')
 november = SHEET.worksheet('November')
-​
+
 data = fy22.get_all_values()
 data2 = november.get_all_values()
-​
-​
-​
+
+
 @app.route("/")
 def home():
     return render_template("home.html")
-​
-​
+
+
 @app.route("/reports")
 def reports():
     fy22 = SHEET.worksheet('FY22/23')
@@ -35,8 +34,8 @@ def reports():
     sectors = [value[0] for value in values[1:]]
     total = [value[12] for value in values[1:]]
     return render_template("reports.html", sheet_data=[sectors, total])
-​
-​
+
+
 @app.route("/reports-data")
 def reports_data():
     fy22 = SHEET.worksheet('FY22/23')
@@ -45,9 +44,8 @@ def reports_data():
     sectors = [value[0] for value in values[1:]]
     total = [value[12] for value in values[1:]]
     return render_template("reports.html", bar_data=[{'x': sectors, 'y': total, 'type': 'bar'}])
-​
-​
-​
+
+
 @app.route("/timesheets")
 def timesheets():
     november = SHEET.worksheet('November')
