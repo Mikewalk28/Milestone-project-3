2f66f3480d44a5ba630e2d9e18a795528c54856d
Mikewalk28 <>
2023-12-05 19:09:21 +0000

    Replacing add_timesheet onto timesheet page and adding drop down menus

--- overheadmanager/routes.py
+++ overheadmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from overheadmanager import app, db
 import gspread
 from google.oauth2.service_account import Credentials
@@ -28,18 +28,9 @@ def home():
     return render_template("home.html")
 
 
+
 @app.route("/reports")
 def reports():
-    fy22 = SHEET.worksheet('FY22/23')
-    headers = ["Sector", "Total"]
-    values = fy22.get_all_values()
-    sectors = [value[0] for value in values[1:]]
-    total = [value[12] for value in values[1:]]
-    return render_template("reports.html", sheet_data=[sectors, total])
-
-
-@app.route("/reports-data")
-def reports_data():
     fy22 = SHEET.worksheet('FY22/23')
     headers = ["Sector", "Total"]
     values = fy22.get_all_values()
@@ -51,10 +42,21 @@ def reports_data():
 @app.route("/timesheets")
 def timesheets():
     november = SHEET.worksheet('November')
-    data = november.get_all_values()
-    return render_template("timesheets.html", sheet_data=data)
+    values = november.get_all_values()
+    sectors = [value[0] for value in values[1:]]
+    employees = values[0][1:12]
+    return render_template("timesheets.html", sheet_data={'sectors': sectors, 'employees': employees})
 
 
 @app.route("/add_timesheet", methods=["GET", "POST"])
 def add_timesheet():
+    print(request.form)
+    november = SHEET.worksheet('November')
+    values = november.get_all_values()
+    sectors = [value[0] for value in values[1:]]
+    employees = values[0][1:12]
+    selected_employee_index = employees.index(request.form['employee']) + 2
+    selected_sector_index = sectors.index(request.form['sector']) + 2
+    print(f"selected_employee_index {selected_employee_index} - selected_sector_index {selected_sector_index}")
+    november.update_cell(selected_sector_index, selected_employee_index, request.form['hours']) 
     return render_template("add_timesheet.html")
