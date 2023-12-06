7f9540d15187678bd785f3844b3e8b6207e6671b
Mikewalk28 <>
2023-12-06 09:19:19 +0000

    adding comments to code

--- overheadmanager/routes.py
+++ overheadmanager/routes.py
@@ -30,7 +30,12 @@ def home():
 
 
 @app.route("/reports")
-def reports():
+def reports():"""
+    Pull the information from FY22/23 worksheet to get the report data.
+    Use the headers from the sheet to target the axis for the charts.
+    Update the sheet to show the reports output.
+    Credit: https://plotly.com/python/basic-charts/
+    """
     fy22 = SHEET.worksheet('FY22/23')
     headers = ["Sector", "Total"]
     values = fy22.get_all_values()
@@ -50,6 +55,12 @@ def timesheets():
 
 @app.route("/add_timesheet", methods=["GET", "POST"])
 def add_timesheet():
+    """
+    Pull the information from Nobember worksheet to get the employee data.
+    Use the index function to match the employee to the sector and post.
+    Update the sheet to show the hours worked.
+    Credit:
+    """
     print(request.form)
     november = SHEET.worksheet('November')
     values = november.get_all_values()
@@ -60,3 +71,23 @@ def add_timesheet():
     print(f"selected_employee_index {selected_employee_index} - selected_sector_index {selected_sector_index}")
     november.update_cell(selected_sector_index, selected_employee_index, request.form['hours']) 
     return render_template("add_timesheet.html")
+
+
+@app.route("/edit_timesheet", methods=["GET", "POST"])
+def edit_timesheet():
+    """
+    Pull the information from Nobember worksheet to get the employee data.
+    Use the index function to match the employee to the sector and post.
+    Update the sheet to show the hours worked.
+    Credit:
+    """
+    print(request.form)
+    november = SHEET.worksheet('November')
+    values = november.get_all_values()
+    sectors = [value[0] for value in values[1:]]
+    employees = values[0][1:12]
+    selected_employee_index = employees.index(request.form['employee']) + 2
+    selected_sector_index = sectors.index(request.form['sector']) + 2
+    print(f"selected_employee_index {selected_employee_index} - selected_sector_index {selected_sector_index}")
+    november.update_cell(selected_sector_index, selected_employee_index, request.form['hours']) 
+    return render_template("edit_timesheet.html")
